723e082b09f9b0beaf22fe3bf83809e1d449bd56
Ariel Toom <ariel.toom@gmail.com>
2024-06-06 13:09:19 +0000

    fixed allowed hosts

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -61,7 +61,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['localhost', 'ALLOWED_HOST', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', os.environ.get('ALLOWED_HOST'), '127.0.0.1']
 
 
 # Application definition
