0fc21f95f33d6ef65ddec9c42904c99de4380431
Ariel Toom <ariel.toom@gmail.com>
2024-06-06 07:41:41 +0000

    permissions

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -4,11 +4,16 @@ from .models import Profile
 
 class ProfileSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
+    is_owner = serializers.SerializerMethodField()
+
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
 
     class Meta:
         model = Profile
         fields = [
             'id', 'owner', 'created_at',
             'updated_at', 'name', 'content',
-            'image'
+            'image', 'is_owner'
         ]
