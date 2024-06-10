7c516eed7f34f258963bf03a8f04e2b405cc31cd
Ariel Toom <ariel.toom@gmail.com>
2024-06-10 13:06:39 +0000

    trying cors allwed

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -109,6 +109,10 @@ if "CLIENT_ORIGIN" in os.environ:
 
 CORS_ALLOWED_ORIGIN_REGEXES = [r"^https://.*\.codeinstitute-ide\.net$",]
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
+
 CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = 'drf_api.urls'
