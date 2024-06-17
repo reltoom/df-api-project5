04a4dbcc042592fb20d1e40ec7b4cd31bb7edc5f
Ariel Toom <ariel.toom@gmail.com>
2024-06-17 11:19:48 +0000

    migrated

--- profiles/migrations/0001_initial.py
+++ profiles/migrations/0001_initial.py
@@ -22,7 +22,7 @@ class Migration(migrations.Migration):
                 ('update_at', models.DateTimeField(auto_now=True)),
                 ('name', models.CharField(blank=True, max_length=225)),
                 ('content', models.TextField(blank=True)),
-                ('image', models.ImageField(default='../default_profile_vhiaki', upload_to='images/')),
+                ('image', models.ImageField(default='../default_profile_fkbq0f', upload_to='images/')),
                 ('owner', models.OneToOneField(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
             ],
             options={
