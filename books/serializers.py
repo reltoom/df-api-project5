b159941836256d17af46173b59ed1d4f567775ec
Ariel Toom <ariel.toom@gmail.com>
2024-06-27 12:00:09 +0000

    added profile connection to serializer

--- books/serializers.py
+++ books/serializers.py
@@ -2,7 +2,11 @@ from rest_framework import serializers
 from .models import Book
 
 class BookSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+    profile_id = serializers.ReadOnlyField(source='owner.profile.id')
+    profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+
     class Meta:
         model = Book
-        fields = ['id', 'owner', 'title', 'author', 'link', 'created_at']
+        fields = ['id', 'owner', 'title', 'author', 'link', 'created_at', 'profile_id', 'profile_image']
         read_only_fields = ['owner', 'created_at']
